2aa3ef45d89283719198d51acdc02bd7d63fe099
verizm <myemail@mail.ru>
2024-11-20 16:56:43 +0300

    add status_up and _calculate_next_status tests with method

--- hospital.py
+++ hospital.py
@@ -3,8 +3,17 @@ class Hospital:
         self._hospital_db = hospital_db
         self._statuses_model = statuses_model
 
+    def _calculate_next_status(self, patient_index: int) -> int:
+        statuses_ids = list(self._statuses_model)
+        current_status_id = self._hospital_db[patient_index]
+        return statuses_ids[statuses_ids.index(current_status_id) + 1]
+
     def get_status(self, patient_id: int) -> str:
         patient_index = patient_id - 1
         status_code = self._hospital_db[patient_index]
         return self._statuses_model[status_code]
 
+    def status_up(self, patient_id: int):
+        patient_index = patient_id - 1
+
+        self._hospital_db[patient_index] = self._calculate_next_status(patient_index)
--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -5,5 +5,21 @@ class TestHospital:
 
     def test_get_status(self):
         statuses = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
         hospital = Hospital([1, None, 2], statuses)
         assert hospital.get_status(patient_id=1) == "Болен"
+
+    def test_status_up(self):
+        statuses = {10: "Тяжело болен", 20: "Болен", 30: "Слегка болен", 40: "Готов к выписке"}
+
+        hospital = Hospital([10, None, 30], statuses)
+        hospital.status_up(patient_id=3)
+
+        assert hospital._hospital_db == [10, None, 40]
+
+    def test_calculate_next_status(self):
+        statuses = {10: "Тяжело болен", 20: "Болен", 30: "Слегка болен", 40: "Готов к выписке"}
+
+        hospital = Hospital([10, None, 30], statuses)
+
+        assert hospital._calculate_next_status(patient_index=2) == 40
