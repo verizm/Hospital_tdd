176e1874a381f64d4e6998d8af6388cc922b05bc
verizm <myemail@mail.ru>
2024-11-20 17:04:17 +0300

    add status_up in Commands tests and class

--- commands.py
+++ commands.py
@@ -1,10 +1,15 @@
 class Commands:
-    def __init__(self, hospital, io_helper):
+    def __init__(self, hospital, communication_with_user):
         self._hospital = hospital
-        self.io_helper = io_helper
+        self._communication_with_user = communication_with_user
 
     def get_status(self):
-        patient_id = self.io_helper.request_patient_id()
+        patient_id = self._communication_with_user.request_patient_id()
         status = self._hospital.get_status(patient_id)
-        self.io_helper.send_status(status)
+        self._communication_with_user.send_status(status)
 
+    def status_up(self):
+        patient_id = self._communication_with_user.request_patient_id()
+        self._hospital.status_up(patient_id)
+        status = self._hospital.get_status(patient_id)
+        self._communication_with_user.send_new_status(status)
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -16,3 +16,16 @@ class TestCommands:
         cmd.get_status()
 
         communication_with_user.send_status.assert_called_with('Готов к выписке')
+
+    def test_status_up(self):
+        communication_with_user = MagicMock()
+        statuses = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
+        hospital = Hospital([2, 0, 1], statuses)
+        cmd = Commands(hospital, communication_with_user)
+
+        communication_with_user.request_patient_id = MagicMock(return_value=1)
+        cmd.status_up()
+
+        communication_with_user.send_new_status.assert_called_with('Готов к выписке')
+        assert hospital._hospital_db == [3, 0, 1]
