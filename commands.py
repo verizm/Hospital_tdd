1bafccb861155dfdcabe59c4a42aaaf5b13caae4
verizm <myemail@mail.ru>
2024-11-21 11:35:05 +0300

    add method tests and try except for statu_up in Commands

--- commands.py
+++ commands.py
@@ -1,3 +1,9 @@
+from hospital_exceptions import (
+    PatientIsNotExistsError,
+    PatientIdIsNotPositiveIntegerError,
+)
+
+
 class Commands:
     def __init__(self, hospital, user_interaction):
         self._hospital = hospital
@@ -9,18 +15,21 @@ class Commands:
         self._user_interaction.send_status(status)
 
     def status_up(self):
-        patient_id = self._user_interaction.request_patient_id()
-        if self._hospital.can_status_up(patient_id):
-            self._hospital.status_up(patient_id)
-            status = self._hospital.get_status(patient_id)
-            self._user_interaction.send_new_status(status)
-        else:
-            if self._user_interaction.request_need_to_discharge():
-                self._hospital.discharge(patient_id)
-                self._user_interaction.send_patient_discharged()
-            else:
+        try:
+            patient_id = self._user_interaction.request_patient_id()
+            if self._hospital.can_status_up(patient_id):
+                self._hospital.status_up(patient_id)
                 status = self._hospital.get_status(patient_id)
-                self._user_interaction.send_status_not_changed(status)
+                self._user_interaction.send_new_status(status)
+            else:
+                if self._user_interaction.request_need_to_discharge():
+                    self._hospital.discharge(patient_id)
+                    self._user_interaction.send_patient_discharged()
+                else:
+                    status = self._hospital.get_status(patient_id)
+                    self._user_interaction.send_status_not_changed(status)
+        except (PatientIsNotExistsError, PatientIdIsNotPositiveIntegerError) as error:
+            self._user_interaction.report_message(error.message)
 
     def calculate_statistic(self):
         total_count_patients = self._hospital.calculate_total_count_patients()
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,6 +2,10 @@ from unittest.mock import MagicMock
 
 from commands import Commands
 from hospital import Hospital
+from hospital_exceptions import (
+    PatientIdIsNotPositiveIntegerError,
+    PatientIsNotExistsError,
+)
 
 
 class TestCommands:
@@ -29,6 +33,30 @@ class TestCommands:
 
         user_interaction.send_new_status.assert_called_with('Готов к выписке')
         assert hospital._hospital_db == [3, 0, 1]
+
+    def test_status_up_when_patient_id_is_not_positive_integer(self):
+        user_interaction = MagicMock()
+        statuses = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
+        hospital = Hospital([2, 0], statuses)
+        cmd = Commands(hospital, user_interaction)
+        user_interaction.request_patient_id = MagicMock(side_effect=PatientIdIsNotPositiveIntegerError)
+
+        cmd.status_up()
+
+        user_interaction.report_message.assert_called_with(PatientIdIsNotPositiveIntegerError().message)
+
+    def test_status_up_when_patient_not_exists(self):
+        user_interaction = MagicMock()
+        statuses = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
+        hospital = Hospital([2, 0], statuses)
+        cmd = Commands(hospital, user_interaction)
+        user_interaction.request_patient_id = MagicMock(return_value=3)
+
+        cmd.status_up()
+
+        user_interaction.report_message.assert_called_with(PatientIsNotExistsError().message)
 
     def test_status_up_when_status_too_high_and_patient_discharged(self):
         user_interaction = MagicMock()
