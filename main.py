9ed11c0ecbfc439bdffa58afa107f865a03919ca
verizm <myemail@mail.ru>
2024-11-21 14:59:00 +0300

    add main

---
+++ main.py
@@ -0,0 +1,16 @@
+from application import Application
+from hospital import Hospital
+from commands import Commands
+from user_interaction import UserInteraction
+from console import Console
+
+if __name__ == '__main__':
+    data_base = [1 for _ in range(200)]
+    statuses_model = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
+    user_interaction = UserInteraction(Console())
+
+    hospital = Hospital(data_base, statuses_model)
+    commands = Commands(hospital, user_interaction)
+
+    Application(commands, user_interaction).run()
